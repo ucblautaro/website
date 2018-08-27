f54ffa56e575f8eda62a35eebef3d6ed81725aad
ucblautaro <42308117+ucblautaro@users.noreply.github.com>
2018-08-26 22:04:11 -0300

    Add calendar folders

---
+++ caleandar-master/js/demo.js
@@ -0,0 +1,26 @@
+var events = [
+  {'Date': new Date(2018, 6-1, 23), 'Title': 'Pr. Christian Aracena - Taller de consejería bíblica','Link':'http://ieucblautaro.cl'},
+  {'Date': new Date(2018, 6-1, 23), 'Title': 'Pr. Christian Aracena - Taller de consejería bíblica','Link':'http://ieucblautaro.cl'},
+  {'Date': new Date(2018, 8-1, 4), 'Title': 'Pr. Iván Reyes - Taller de ideología de género','Link':'http://ieucblautaro.cl'},
+  {'Date': new Date(2018, 8-1, 25), 'Title': 'Taller "Orando las escrituras" 17hrs.','Link':'http://ieucblautaro.cl'},
+  {'Date': new Date(2018, 8-1, 26), 'Title': 'Escuela dominical y CFT','Link':'http://ieucblautaro.cl'},
+
+
+  {'Date': new Date(2018, 8-1, 22), 'Title': 'Culto de oración  20:00 hrs.','Link':'http://ieucblautaro.cl'},
+  {'Date': new Date(2018, 8-1, 29), 'Title': 'Culto de oración  20:00 hrs.','Link':'http://ieucblautaro.cl'},
+
+  {'Date': new Date(2018, 9-1, 2), 'Title': 'Escuela dominical 10:30 hrs.','Link':'http://ieucblautaro.cl'},
+  {'Date': new Date(2018, 9-1, 16), 'Title': 'Escuela dominical 10:30 hrs.','Link':'http://ieucblautaro.cl'},
+  {'Date': new Date(2018, 9-1, 16), 'Title': 'Escuela dominical 10:30 hrs.','Link':'http://ieucblautaro.cl'},
+  {'Date': new Date(2018, 9-1, 23), 'Title': 'Escuela dominical 10:30 hrs.','Link':'http://ieucblautaro.cl'},
+  {'Date': new Date(2018, 9-1, 30), 'Title': 'Escuela dominical 10:30 hrs.','Link':'http://ieucblautaro.cl'},
+
+  {'Date': new Date(2018, 9-1, 5), 'Title': 'Culto de oración  20:00 hrs.','Link':'http://ieucblautaro.cl'},
+  {'Date': new Date(2018, 9-1, 12), 'Title': 'Culto de oración  20:00 hrs.','Link':'http://ieucblautaro.cl'},
+  {'Date': new Date(2018, 9-1, 19), 'Title': 'Culto de oración  20:00 hrs.','Link':'http://ieucblautaro.cl'},
+  {'Date': new Date(2018, 9-1, 26), 'Title': 'Culto de oración  20:00 hrs.','Link':'http://ieucblautaro.cl'},
+
+];
+var settings = {};
+var element = document.getElementById('caleandar');
+caleandar(element, events, settings);
