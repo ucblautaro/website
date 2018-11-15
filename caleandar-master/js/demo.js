d5f1aad62fe7529d0ac9d4013166f3c71732c5fb
Hans Topp <htopp2016@alu.uct.cl>
2018-11-14 23:01:32 -0300

    changes

--- caleandar-master/js/demo.js
+++ caleandar-master/js/demo.js
@@ -1,50 +1,5 @@
 var events = [
   //AÑO, MES, DIA
-  {'Date': new Date(2018, 6-1, 23), 'Title': 'Pr. Christian Aracena - Taller de consejería bíblica','Link':'http://ieucblautaro.cl'},
-  {'Date': new Date(2018, 6-1, 23), 'Title': 'Pr. Christian Aracena - Taller de consejería bíblica','Link':'http://ieucblautaro.cl'},
-  {'Date': new Date(2018, 8-1, 4), 'Title': 'Pr. Iván Reyes - Taller de ideología de género','Link':'http://ieucblautaro.cl'},
-  {'Date': new Date(2018, 8-1, 25), 'Title': 'Taller "Orando las escrituras" 17hrs.','Link':'http://ieucblautaro.cl'},
-  {'Date': new Date(2018, 8-1, 26), 'Title': 'Escuela dominical y CFT','Link':'http://ieucblautaro.cl'},
-
-
-  {'Date': new Date(2018, 8-1, 22), 'Title': 'Culto de oración  20:00 hrs.','Link':'http://ieucblautaro.cl'},
-  {'Date': new Date(2018, 8-1, 29), 'Title': 'Culto de oración  20:00 hrs.','Link':'http://ieucblautaro.cl'},
-
-  //                                                    SEPTIEMBRE
-  //ESC DOM
-  {'Date': new Date(2018, 9-1, 2), 'Title': 'Escuela dominical 10:30 hrs.','Link':'http://ieucblautaro.cl'},
-  {'Date': new Date(2018, 9-1, 9), 'Title': 'Escuela dominical 10:30 hrs.','Link':'http://ieucblautaro.cl'},
-  {'Date': new Date(2018, 9-1, 16), 'Title': 'Escuela dominical 10:30 hrs.','Link':'http://ieucblautaro.cl'},
-  {'Date': new Date(2018, 9-1, 16), 'Title': 'Escuela dominical 10:30 hrs.','Link':'http://ieucblautaro.cl'},
-  {'Date': new Date(2018, 9-1, 23), 'Title': 'Escuela dominical 10:30 hrs.','Link':'http://ieucblautaro.cl'},
-  {'Date': new Date(2018, 9-1, 30), 'Title': 'Escuela dominical y aniversario 10:30 hrs.','Link':'http://ieucblautaro.cl'},
-  //ORAC
-  {'Date': new Date(2018, 9-1, 5), 'Title': 'Culto de oración  20:00 hrs.','Link':'http://ieucblautaro.cl'},
-  {'Date': new Date(2018, 9-1, 12), 'Title': 'Culto de oración  20:00 hrs.','Link':'http://ieucblautaro.cl'},
-  {'Date': new Date(2018, 9-1, 19), 'Title': 'Culto de oración  20:00 hrs.','Link':'http://ieucblautaro.cl'},
-  {'Date': new Date(2018, 9-1, 26), 'Title': 'Culto de oración  20:00 hrs.','Link':'http://ieucblautaro.cl'},
-  //OTROS
-  {'Date': new Date(2018, 9-1, 29), 'Title': 'Taller "Orando las escrituras" 17hrs.','Link':'http://ieucblautaro.cl'},
-
-  //                                                    OCTUBRE
-  //ESC DOM
-  {'Date': new Date(2018, 10-1, 7), 'Title': 'Escuela dominical 10:30 hrs.','Link':'http://ieucblautaro.cl'},
-  {'Date': new Date(2018, 10-1, 14), 'Title': 'Escuela dominical 10:30 hrs.','Link':'http://ieucblautaro.cl'},
-  {'Date': new Date(2018, 10-1, 21), 'Title': 'Escuela dominical 10:30 hrs.','Link':'http://ieucblautaro.cl'},
-  {'Date': new Date(2018, 10-1, 28), 'Title': 'Escuela dominical 10:30 hrs.','Link':'http://ieucblautaro.cl'},
-    //ORAC
-  {'Date': new Date(2018, 10-1, 3), 'Title': 'Culto de oración  20:00 hrs.','Link':'http://ieucblautaro.cl'},
-  {'Date': new Date(2018, 10-1, 10), 'Title': 'Culto de oración  20:00 hrs.','Link':'http://ieucblautaro.cl'},
-  {'Date': new Date(2018, 10-1, 17), 'Title': 'Culto de oración  20:00 hrs.','Link':'http://ieucblautaro.cl'},
-  {'Date': new Date(2018, 10-1, 24), 'Title': 'Culto de oración  20:00 hrs.','Link':'http://ieucblautaro.cl'},
-  {'Date': new Date(2018, 10-1, 31), 'Title': 'Culto de oración  20:00 hrs.','Link':'http://ieucblautaro.cl'},
-
-  //OTROS
-  {'Date': new Date(2018, 10-1, 27), 'Title': 'Taller "Orando las escrituras" 17hrs.','Link':'http://ieucblautaro.cl'},
-  {'Date': new Date(2018, 10-1, 6), 'Title': 'Conferencia de evangelismo 14hrs','Link':'http://ieucblautaro.cl'},
-  {'Date': new Date(2018, 10-1, 7), 'Title': 'Conferencia de evangelismo 14hrs','Link':'http://ieucblautaro.cl'},
-
-  
   //                                                    NOVIEMBRE
   //ESC DOM
   {'Date': new Date(2018, 11-1, 4), 'Title': 'Escuela dominical 10:30 hrs.','Link':'http://ieucblautaro.cl'},
@@ -58,13 +13,13 @@ var events = [
   {'Date': new Date(2018, 11-1, 28), 'Title': 'Culto de oración  20:00 hrs.','Link':'http://ieucblautaro.cl'},
   //OTROS
   {'Date': new Date(2018, 11-1, 17), 'Title': 'Encuentro familiar en UCB Bethel y Encuentro de jóvenes en UCB Pitrufquén','Link':'http://ieucblautaro.cl'},
-  {'Date': new Date(2018, 11-1, 30), 'Title': 'CONFERENCIA CAMINAMOS POR UNA SENDA MARCADA - Desde las 18hrs.','Link':'http://ieucblautaro.cl'},
+  {'Date': new Date(2018, 11-1, 30), 'Title': 'CONFERENCIA - Desde las 18hrs.','Link':'http://ieucblautaro.cl'},
     
   
   
   //                                                    DICIEMBRE
   //ESC DOM
-  {'Date': new Date(2018, 12-1, 2), 'Title': 'CONFERENCIA CAMINAMOS POR UNA SENDA MARCADA - Desde las 10hrs. (No hay escuela dominical)','Link':'http://ieucblautaro.cl'},
+  {'Date': new Date(2018, 12-1, 2), 'Title': 'CONFERENCIA - Desde las 10hrs (No hay escuela dominical)','Link':'http://ieucblautaro.cl'},
   {'Date': new Date(2018, 12-1, 9), 'Title': 'Escuela dominical 10:30 hrs.','Link':'http://ieucblautaro.cl'},
   {'Date': new Date(2018, 12-1, 16), 'Title': 'Escuela dominical 10:30 hrs.','Link':'http://ieucblautaro.cl'},
   {'Date': new Date(2018, 12-1, 23), 'Title': 'Escuela dominical 10:30 hrs.','Link':'http://ieucblautaro.cl'},
@@ -75,9 +30,9 @@ var events = [
   {'Date': new Date(2018, 12-1, 19), 'Title': 'Culto de oración  20:00 hrs.','Link':'http://ieucblautaro.cl'},
   {'Date': new Date(2018, 12-1, 26), 'Title': 'Culto de oración  20:00 hrs.','Link':'http://ieucblautaro.cl'},
   //OTROS
-  {'Date': new Date(2018, 12-1, 2), 'Title': 'CONFERENCIA CAMINAMOS POR UNA SENDA MARCADA- Desde las 10hrs.','Link':'http://ieucblautaro.cl'},
+  {'Date': new Date(2018, 12-1, 1), 'Title': 'CONFERENCIA - Desde las 10hrs.','Link':'http://ieucblautaro.cl'},
     
- 
+
 ];
 var settings = {};
 var element = document.getElementById('caleandar');
